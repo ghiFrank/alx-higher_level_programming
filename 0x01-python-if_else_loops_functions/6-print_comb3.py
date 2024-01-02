bbfed90f50935e1121bb005ceddbb2df717f272e
ghifrank <ayoub3412@gmail.com>
2024-01-02 19:08:54 +0100

    y

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for x in range(0, 10):
+    for n in range(x + 1, 10):
+        if x == 8 and n == 9:
+            print("{}{}".format(x,n), end='\n')
+            continue
+        print("{}{}".format(x,n), end=', ')
