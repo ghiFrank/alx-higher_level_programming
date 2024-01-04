956f6e3058d925a8b705ee9f684c7c910815a81e
ghifrank <ayoub3412@gmail.com>
2024-01-04 21:48:49 +0100

    y

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from sys import argv
-argument = "argument" if len(argv) == 2 else "arguments"
-end = "." if len(argv) == 1 else ":"
-print("{} {}{}".format(len(argv) - 1, argument, end))
-if len(argv) > 1:
-    for n, arg in enumerate(argv[1:], 1):
-        print("{}: {}".format(n, arg))
+if __name__ == "__main__":
+    from sys import argv
+    argument = "argument" if len(argv) == 2 else "arguments"
+    end = "." if len(argv) == 1 else ":"
+    print("{} {}{}".format(len(argv) - 1, argument, end))
+    if len(argv) > 1:
+        for n, arg in enumerate(argv[1:], 1):
+            print("{}: {}".format(n, arg))
