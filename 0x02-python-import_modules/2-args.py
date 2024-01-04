99a15a079f0bde3f52f12e183fa4fd147f12f27f
ghifrank <ayoub3412@gmail.com>
2024-01-04 21:46:47 +0100

    y

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,5 +4,5 @@ argument = "argument" if len(argv) == 2 else "arguments"
 end = "." if len(argv) == 1 else ":"
 print("{} {}{}".format(len(argv) - 1, argument, end))
 if len(argv) > 1:
-    for n,arg in enumerate(argv[1:],1):
+    for n, arg in enumerate(argv[1:], 1):
         print("{}: {}".format(n, arg))
