43edd4f93f9e5cb7a95ba9e904c15e040ebd8634
ghifrank <ayoub3412@gmail.com>
2024-01-08 18:32:03 +0100

    y

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
     for n in matrix:
-        for i in n:
-            endo = "" if i == len(n) else " "
-            print('{:d}'.format(i), end = endo)
-        print('\n')
+        if len(n) == 0:
+            print()
+        for i in range(len(n)):
+            print('{:d}'.format(n[i]), end = "\n" if i is len(n) - 1 else " ")
