2c7b6058769991f0c9cc2c89c0edbcb05bfc8bb4
ghifrank <ayoub3412@gmail.com>
2024-01-10 22:28:08 +0100

    y

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if isinstance(roman_string, str) is False or roman_string is None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
     num = 0
+    total = 0
     dictio = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    for e, n in enumerate(roman_string):
-        if n is not roman_string[0]:
-            if roman_string[e - 1] == 'I' and (n == 'V' or n == 'X'):
-                num = num + dictio[n] - 2
-                continue
-            if roman_string[e - 1] == 'X' and (n == 'L' or n == 'C'):
-                num = num + dictio[n] - 20
-                continue
-            if roman_string[e - 1] == 'C' and (n == 'D' or n == 'M'):
-                num = num + dictio[n] - 200
-                continue
-        num += dictio[n]
-    return num
+    for n in reversed(roman_string):
+        num = dictio[n]
+        total += num if total < num * 5 else -num
+    return total
+
+roman_number = "IVI"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "MCM"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "MCMXLIV"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "LXXXVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
