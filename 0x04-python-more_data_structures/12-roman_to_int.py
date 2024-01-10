a9e7728041748b39630a3e507821a664dc38bb81
ghifrank <ayoub3412@gmail.com>
2024-01-10 22:02:08 +0100

    y

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    num = 0
+    dictio ={'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for e, n in enumerate(roman_string):
+        if n is not roman_string[0]:
+            if roman_string[e - 1] == 'I' and  (n == 'V' or n == 'X'):
+                num = num + dictio[n] - 2
+                continue
+            if roman_string[e - 1] == 'X' and  (n == 'L' or n == 'C'):
+                num = num + dictio[n] - 20
+                continue
+            if roman_string[e - 1] == 'C' and  (n == 'D' or n == 'M'):
+                num = num + dictio[n] - 200
+                continue
+        num += dictio[n]
+    return num
