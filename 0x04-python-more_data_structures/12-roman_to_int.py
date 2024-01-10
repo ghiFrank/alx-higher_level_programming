5b1d0bb7e926431149fb95d2fa97d00a01a9920c
ghifrank <ayoub3412@gmail.com>
2024-01-10 22:08:45 +0100

    y

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,8 @@ def roman_to_int(roman_string):
     if isinstance(roman_string, str) is False or roman_string is None:
         return 0
     num = 0
-    dictio ={'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    dictio ={'I': 1, 'V': 5, 'X': 10, 'L': 50, 
+             'C': 100, 'D': 500, 'M': 1000}
     for e, n in enumerate(roman_string):
         if n is not roman_string[0]:
             if roman_string[e - 1] == 'I' and  (n == 'V' or n == 'X'):
