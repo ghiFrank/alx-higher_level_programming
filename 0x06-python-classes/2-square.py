7b4e626bba1c0d6f4962d8974f18844e4ff988f3
ghifrank <ayoub3412@gmail.com>
2024-01-23 12:48:20 +0100

    y

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,11 +3,14 @@
 
 
 class Square:
-    """defines a square"""
+    """defines a square."""
+
     def __init__(self, size=0):
-        """constructor
+        """constructor.
+
         Args:
-            size: length of a side of the square
+            size: length of a side of the square.
+
         Raises:
             TypeError: if size is not an integer
             ValueError: if size is less than 0
