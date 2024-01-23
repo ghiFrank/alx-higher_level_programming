381be8c6f31491d6636affbc4449bb055e5c5474
ghifrank <ayoub3412@gmail.com>
2024-01-23 12:41:17 +0100

    y

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,10 +3,10 @@
 
 
 class Square:
-	"""defines a square"""
-	def __init__(self, size):
-		"""constructor
-		Args:
-			size: length of a side of the square
-		"""
-		self.__size = size
+    """defines a square"""
+    def __init__(self, size):
+        """constructor
+        Args:
+        	size: length of a side of the square
+        """
+        self.__size = size
