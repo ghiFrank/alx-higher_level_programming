cc9699017cc3228c72b4d5bf955757e3ee5773dc
ghifrank <ayoub3412@gmail.com>
2024-02-05 15:36:57 +0100

    y

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,11 +7,11 @@ def MyInt(int):
     def __new__(cls, *args, **kwargs):
         """create a new instance of the class"""
         return super(MyInt, cls).__new__(cls, *args, **kwargs)
-    
+
     def __eq__(self, other):
         """what was != is now =="""
         return int(self) != other
-    
+
     def __ne__(self, other):
         """what was == is now !="""
         return int(self) == other
