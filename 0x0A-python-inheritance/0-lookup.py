82d8f94ddf62686312bed6f6c3964870b54b7db4
ghifrank <ayoub3412@gmail.com>
2024-02-05 13:28:19 +0100

    y

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+'''Module For lookup'''
+
 
 def lookup(obj):
+    '''Looks for available attributes and methods of an object.
+    Args:
+        obj (object): the object.
+    Returns:
+        List: the list of attributes
+    '''
     return dir(obj)
