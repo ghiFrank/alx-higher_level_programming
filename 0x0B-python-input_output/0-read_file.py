499c6d8d273f3183e5082819e09ff360e5a577e3
ghifrank <ayoub3412@gmail.com>
2024-02-06 10:15:34 +0100

    y

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""read_file module"""
+
+
+def read_file(filename=""):
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            print(line, end='')
