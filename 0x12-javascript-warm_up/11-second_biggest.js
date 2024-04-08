0dc445b90bab581fdb4b8b2fe6341da8b7311102
ghifrank <ayoub3412@gmail.com>
2024-04-08 22:30:30 +0000

    y

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,8 +2,9 @@
 // searches the second biggest integer in the list of arguments.
 
 if (process.argv.length <= 3) {
-    console.log(0);
-  } else {
-    const list = process.argv.sort();
-    console.log(list.reverse()[1]);
-  }
+  console.log('0');
+} else {
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
+}
