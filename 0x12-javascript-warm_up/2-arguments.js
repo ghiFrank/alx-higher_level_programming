bccb54206a96a595cc10f9e253eb318682b1b0b7
ghifrank <ayoub3412@gmail.com>
2024-04-08 22:22:24 +0000

    y

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 // Prints a message depending of the number of arguments passed
 
-if (process.argv.length === 2) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
-} else if (process.argv.length === 3) {
-  console.log('Argument found');
 } else {
-  console.log('Arguments found');
+  console.log(process.argv[2]);
 }
