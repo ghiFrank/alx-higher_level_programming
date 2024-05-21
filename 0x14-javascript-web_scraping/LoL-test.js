4508081f5d06154279477e3ab72939d42222a4e9
ghifrank <ayoub3412@gmail.com>
2024-05-21 13:55:50 +0100

    y

--- 0x14-javascript-web_scraping/LoL-test.js
+++ 0x14-javascript-web_scraping/LoL-test.js
@@ -3,7 +3,6 @@
 const request = require('request');
 const gameName = process.argv[2];
 const id = process.argv[3];
-const apiKey = 'RGAPI-4f4bd076-2cda-4018-99f6-b4958deb6cd3';  // Replace with your actual API key
 const url = `https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${id}?api_key=RGAPI-4f4bd076-2cda-4018-99f6-b4958deb6cd3`;
 
 
